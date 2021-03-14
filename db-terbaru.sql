62706ae0ab2fcbe44edacb3183a47d5247c4cbbf
Satria Darmawan <satria.darmawan@paxel.co>
2021-03-14 10:54:35 +0700

    new db

---
+++ db-terbaru.sql
@@ -0,0 +1,159 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.6.20 : Database - apriori_resep_makanan
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`apriori_resep_makanan` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `apriori_resep_makanan`;
+
+/*Table structure for table `confidence` */
+
+DROP TABLE IF EXISTS `confidence`;
+
+CREATE TABLE `confidence` (
+  `kombinasi1` varchar(255) DEFAULT NULL,
+  `kombinasi2` varchar(255) DEFAULT NULL,
+  `support_xUy` double DEFAULT NULL,
+  `support_x` double DEFAULT NULL,
+  `confidence` double DEFAULT NULL,
+  `lolos` tinyint(4) DEFAULT NULL,
+  `min_support` double DEFAULT NULL,
+  `min_confidence` double DEFAULT NULL,
+  `nilai_uji_lift` double DEFAULT NULL,
+  `korelasi_rule` varchar(100) DEFAULT NULL,
+  `id_process` int(11) NOT NULL DEFAULT '0',
+  `jumlah_a` int(11) DEFAULT NULL,
+  `jumlah_b` int(11) DEFAULT NULL,
+  `jumlah_ab` int(11) DEFAULT NULL,
+  `px` double DEFAULT NULL,
+  `py` double DEFAULT NULL,
+  `pxuy` double DEFAULT NULL,
+  `from_itemset` int(11) DEFAULT NULL COMMENT 'dari itemset 2/3'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*Data for the table `confidence` */
+
+/*Table structure for table `itemset1` */
+
+DROP TABLE IF EXISTS `itemset1`;
+
+CREATE TABLE `itemset1` (
+  `atribut` varchar(200) DEFAULT NULL,
+  `jumlah` int(11) DEFAULT NULL,
+  `support` double DEFAULT NULL,
+  `lolos` tinyint(4) DEFAULT NULL,
+  `id_process` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `itemset1` */
+
+/*Table structure for table `itemset2` */
+
+DROP TABLE IF EXISTS `itemset2`;
+
+CREATE TABLE `itemset2` (
+  `atribut1` varchar(200) DEFAULT NULL,
+  `atribut2` varchar(200) DEFAULT NULL,
+  `jumlah` int(11) DEFAULT NULL,
+  `support` double DEFAULT NULL,
+  `lolos` tinyint(4) DEFAULT NULL,
+  `id_process` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `itemset2` */
+
+/*Table structure for table `itemset3` */
+
+DROP TABLE IF EXISTS `itemset3`;
+
+CREATE TABLE `itemset3` (
+  `atribut1` varchar(200) DEFAULT NULL,
+  `atribut2` varchar(200) DEFAULT NULL,
+  `atribut3` varchar(200) DEFAULT NULL,
+  `jumlah` int(11) DEFAULT NULL,
+  `support` double DEFAULT NULL,
+  `lolos` tinyint(4) DEFAULT NULL,
+  `id_process` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `itemset3` */
+
+/*Table structure for table `process_log` */
+
+DROP TABLE IF EXISTS `process_log`;
+
+CREATE TABLE `process_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `start_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL,
+  `min_support` double DEFAULT NULL,
+  `min_confidence` double DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `process_log` */
+
+/*Table structure for table `temporary` */
+
+DROP TABLE IF EXISTS `temporary`;
+
+CREATE TABLE `temporary` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `menu` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `temporary` */
+
+insert  into `temporary`(`id`,`menu`) values (4,'tomat'),(5,'jeruk'),(6,'tepung'),(7,'garam'),(8,'kelapa');
+
+/*Table structure for table `transaksi` */
+
+DROP TABLE IF EXISTS `transaksi`;
+
+CREATE TABLE `transaksi` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `transaction_date` date DEFAULT NULL,
+  `produk` text,
+  `menu` text,
+  `ref` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `transaksi` */
+
+insert  into `transaksi`(`id`,`transaction_date`,`produk`,`menu`,`ref`) values (1,'2021-05-02','Pisang Kepok,Tepung Ketan,Tepung Beras,Garam,Kelapa Parut,Gula Merah,Daun Pandan','Lapek Barajuik','https://cookpad.com/id/resep/14355632-cara-membuat-lapek-barajuik-makanan-tradisional-di-sumbar'),(2,'2021-05-03','Beras Ketan,Santan,Daun Pandan,Garam,Ikan Tongkol Kukus,Daun Jeruk,Serai,Kemangi,Jeruk Nipis,Tomat,Kaldu Bubuk,Merica Bubuk,Gula Pasir,Minyak Goreng,Daun Pisang,Cabai Rawit','Lalampa khas Manado','https://cookpad.com/id/resep/14203324-lalampa-khas-manado-kudapan-tradisional-nusantara');
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(200) DEFAULT NULL,
+  `nama` varchar(200) DEFAULT NULL,
+  `password` text,
+  `level` tinyint(4) NOT NULL DEFAULT '0',
+  `last_login` datetime DEFAULT NULL,
+  `inactive` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`username`,`nama`,`password`,`level`,`last_login`,`inactive`) values (1,'admin','Administrator','21232f297a57a5a743894a0e4a801fc3',1,'2017-02-22 01:49:04',0),(2,'user','User Direktur','ee11cbb19052e40b07aac0ca060c23ee',0,'2016-05-22 09:19:02',0);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
